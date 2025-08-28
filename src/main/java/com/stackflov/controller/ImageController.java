29b4cbb1782bedaf3d0f3de506a9c9318c5d9527
SeoSeungHyun <seunghyun.seo101@gmail.com>
2025-08-28 21:55:10 +0900

    feat: S3 구현

---
+++ src/main/java/com/stackflov/controller/ImageController.java
@@ -0,0 +1,24 @@
+package com.stackflov.controller;
+
+import com.stackflov.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final S3Service s3Service;
+
+    @PostMapping("/images/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+        String imageUrl = s3Service.upload(multipartFile, "images"); // "images"는 S3 버킷 안의 폴더 이름
+        return ResponseEntity.ok(imageUrl);
+    }
+}
