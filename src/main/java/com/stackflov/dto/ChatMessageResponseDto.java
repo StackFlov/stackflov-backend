d6d61c912c9082ca552295a7d81ba3185472d19e
Seo Seung Hyun <seunghyun.seo101@gmail.com>
2025-12-29 23:31:39 +0900

    feat: 메세지 발신자 이메일 대조를 통한 좌우 로직 구현

--- src/main/java/com/stackflov/dto/ChatMessageResponseDto.java
+++ src/main/java/com/stackflov/dto/ChatMessageResponseDto.java
@@ -11,12 +11,14 @@ public class ChatMessageResponseDto {
     private final Long messageId;
     private final String senderNickname;
     private final String content;
+    private String senderEmail;
     private final LocalDateTime sentAt;
 
     public ChatMessageResponseDto(ChatMessage message) {
         this.messageId = message.getId();
         this.senderNickname = message.getSender().getNickname();
         this.content = message.getContent();
+        this.senderEmail = message.getSender().getEmail();
         this.sentAt = message.getSentAt();
     }
 }
